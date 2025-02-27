6051e82747127aad4809dec6ea645eb5c617655e
Varnyxidus <theodore.foeh@binus.ac.id>
2025-02-27 07:18:04 +0700

    update navbar link

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,11 +1,11 @@
-import { Link } from '@mui/material'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <div className='p-6 border-b flex justify-between bg-orange-600'>
-        <Link href='/' className='text-2xl font-bold text-white' sx={{textDecoration: 'none'}}>TaskMana</Link>
-        <Link href='/create' className='bg-slate-100 grid items-center text-orange-600 py-2 px-4 rounded-full font-bold shadow-md' sx={{textDecoration: 'none'}}>Add Task</Link>
+        <Link href='/' className='text-2xl font-bold text-white'>TaskMana</Link>
+        <Link href='/create' className='bg-slate-100 grid items-center text-orange-600 py-2 px-4 rounded-full font-bold shadow-md'>Add Task</Link>
     </div>
   )
 }
